908c591d6abcc9ae1cc209b2f79f69a59e05a8bc
DarshanPable15 <pable.darshan15@gmail.com>
2018-06-25 19:43:31 +0530

    Basic creation of page

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>My Online Shop</title>
+	<link rel="stylesheet" type="text/css" href="styles/style.css" media="all">
+</head>
+<body>
+	<div class="main_wrapper">
+
+		<div>This is Header!</div>
+
+		<div>This is menu bar!</div>
+
+		<div>This is side bar!</div>
+
+		<div>This is content area!</div>
+
+		<div>This is the footer!</div>
+	</div>
+</body>
+</html>
